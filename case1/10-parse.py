66ca9b1d8d564c75ef6f654f098bc1f25c8dbb09
nardtree <gim.kobayashi@gmail.com>
2018-04-10 13:20:35 +0900

    修正

--- case1/10-parse.py
+++ case1/10-parse.py
@@ -13,8 +13,6 @@ from scipy import sparse
 import pickle
 
 import gzip
-
-import math
 if '--step1' in sys.argv: # feat indexを作成
   fp = open('../../.kaggle/competitions/talkingdata-adtracking-fraud-detection/mnt/ssd/kaggle-talkingdata2/competition_files/train.csv')
 
@@ -29,9 +27,6 @@ if '--step1' in sys.argv: # feat indexを作成
     is_attributed = obj['is_attributed']
     del obj['is_attributed'] 
 
-    ## stop using ip: ipはcategory
-    # ip      = 'ip:' + obj['ip']
-
     # appはcategory
     app     = 'app:' + obj['app']
     # deviceはcategory
@@ -40,31 +35,33 @@ if '--step1' in sys.argv: # feat indexを作成
     os      = 'os:' + obj['os']
     # channelはcategory
     channel = 'channel:' + obj['channel']
-    
+   
     for feat in [app, device, os, channel]:
       if feat_index.get(feat) is None:
         feat_index[feat] = len(feat_index)
  
-  # 手動でこの特徴量を追加を宣言
-  feat_index[ 'ip_freq' ] = len(feat_index)
+  # 手動で追加
   for i in range(10):
-    # このカテゴリに追加して、0 ~ 9まで追加する
     feat_index[ f'ip_cat:{i}' ] = len(feat_index)
+  feat_index[ 'ip_freq_lin' ] = len(feat_index)
   print(feat_index)
 
   json.dump(feat_index, fp=open('files/feat_index.json', 'w'), indent=2)
 
 if '--step2' in sys.argv:
- 
-  ips = {}
-  ips1 = json.load(fp=open('./files/click_ip_freq.json'))
-  ips2 = json.load(fp=open('./files/nclick_ip_freq.json'))
-  for ip, freq in ips1.items():
-    ips[ip] = freq
-  for ip, freq in ips2.items():
-    if ips.get(ip) is None:
-      ips[ip] = 0
-    ips[ip] += freq
+
+  
+  ip_freq = {}
+  ip_freq1 = json.load(fp=open('./files/click_ip_freq.json'))
+  ip_freq2 = json.load(fp=open('./files/nclick_ip_freq.json'))
+  for ip, freq in ip_freq1.items():
+    if ip_freq.get(ip) is None:
+      ip_freq[ip] = 0
+    ip_freq[ip] += freq
+  for ip, freq in ip_freq2.items():
+    if ip_freq.get(ip) is None:
+      ip_freq[ip] = 0
+    ip_freq[ip] += freq
 
   fp = open('../../.kaggle/competitions/talkingdata-adtracking-fraud-detection/mnt/ssd/kaggle-talkingdata2/competition_files/train.csv')
 
@@ -84,9 +81,10 @@ if '--step2' in sys.argv:
 
     del obj['is_attributed'] 
 
-    # ipはcategory
-    ip_cat  = 'ip_cat:%d'%len(f'{ips[ obj["ip"] ]}')
-    ip_freq = 'ip_freq:%f'%( math.log( ips[ obj['ip'] ] ) )
+    ip_cat  = ip_freq[ obj['ip'] ] 
+    ip_cat  = 'ip_cat' + len(str(ip_freq))
+
+    ip_freq_lin = math.log( ip_freq[ obj['ip'] ] )
     # appはcategory
     app     = 'app:' + obj['app']
     # deviceはcategory
@@ -96,10 +94,8 @@ if '--step2' in sys.argv:
     # channelはcategory
     channel = 'channel:' + obj['channel']
    
-    xs = [feat_index[feat] for feat in [ip_cat,  app, device, os, channel]]
-    # adhoc操作であるが, ip_catとip_freqをここに追加
-    xs.insert(0, math.log( ips[ obj['ip'] ] ) )
-    #print(xs) 
+    xs = [feat_index[feat] for feat in [ip, app, device, os, channel]]
+     
     Xs.append( xs ); ys.append( y )
 
   data = gzip.compress( pickle.dumps( (Xs, ys) ) )
