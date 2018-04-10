fc2e7c968b30b57788c76a9cde371ec1c7bcf4db
nardtree <gim.kobayashi@gmail.com>
2018-04-10 19:43:23 +0900

    update

--- case1/30-wrapper.py
+++ case1/30-wrapper.py
@@ -1,4 +1,17 @@
 
 import os
 
-os.system('lightgbm config=files/train.conf')
+import glob
+
+import random
+
+import sys
+confs = glob.glob('./files/confs/*')
+random.shuffle(confs)
+
+conf = confs.pop()
+os.system(f'lightgbm config={conf}') 
+sys.exit() 
+result = os.popen(f'lightgbm config={conf}').read()
+sys.exit() 
+
