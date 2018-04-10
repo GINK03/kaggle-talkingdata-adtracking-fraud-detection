9e452cb479c9c9be76c49d9ae73470184f0645ea
nardtree <gim.kobayashi@gmail.com>
2018-04-10 23:33:56 +0900

    imporve somehing

--- case2/10-parse.py
+++ case2/10-parse.py
@@ -21,11 +21,11 @@ import concurrent.futures
 if '--step1' in sys.argv: # feat indexを作成
   # train
   def _map_train(arg):
-    path = arg
+    key,path = arg
     feats = set()
     for index, line in enumerate(path.open()):
       if index%100000 == 0:
-        print(f'now iter {index}')
+        print(f'now iter {index}@{key}')
       obj  = json.loads(line.strip())
       is_attributed = obj['is_attributed']
       del obj['is_attributed'] 
@@ -42,11 +42,11 @@ if '--step1' in sys.argv: # feat indexを作成
     return feats
   # test
   def _map_test(arg):
-    path = arg
+    key, path = arg
     feats = set()
     for index, line in enumerate(path.open()):
       if index%100000 == 0:
-        print(f'now iter {index}')
+        print(f'now iter {index}@{key}')
       obj  = json.loads(line.strip())
       # appはcategory
       app     = 'app:' + obj['app']
@@ -61,15 +61,15 @@ if '--step1' in sys.argv: # feat indexを作成
     return feats
  
   feat_index = {}
-  paths = list(Path('./files/data/').glob('train_*'))
+  args = [(index,path) for index, path in enumerate(Path('./files/data/').glob('train_*'))]
   with concurrent.futures.ProcessPoolExecutor(max_workers=16) as exe:
-    for _feasts in exe.map(_map_train, paths):
+    for _feats in exe.map(_map_train, args):
       for feat in _feats:
         if feat_index.get(feat) is None:
           feat_index[feat] = len(feat_index)
-  paths = list(Path('./files/data/').glob('test_*'))
+  args = [(index,path) for index, path in enumerate(Path('./files/data/').glob('test_*'))]
   with concurrent.futures.ProcessPoolExecutor(max_workers=16) as exe:
-    for _feasts in exe.map(_map_test, paths):
+    for _feats in exe.map(_map_test, args):
       for feat in _feats:
         if feat_index.get(feat) is None:
           feat_index[feat] = len(feat_index)
@@ -97,65 +97,73 @@ if '--step2' in sys.argv:
   feat_index = json.load(fp=open('files/feat_index.json'))
   
   # test data
-  fp = open('../../.kaggle/competitions/talkingdata-adtracking-fraud-detection/test.csv')
-  head = next(fp).strip().split(',')
-  Xs, ys = [], []
-  for index, line in enumerate(fp):
-    if index%100000 == 0:
-      print(f'now test iter {index}')
-    val = line.strip().split(',')
-    obj = dict( zip(head, val) )
+  def _map_test(arg):
+    key, path = arg
+    Xs, ys = [], []
+    for index, line in enumerate(path.open()):
+      if index%100000 == 0:
+        print(f'now test iter {index}@{key}')
+      obj = json.loads(line.strip())
 
-    ip_cat  = ip_freq[ obj['ip'] ] 
-    ip_cat  = 'ip_cat:{}'.format( len(str(ip_cat)) )
-    ip_freq_lin = math.log( ip_freq[ obj['ip'] ] )
-    # appはcategory
-    app     = 'app:' + obj['app']
-    # deviceはcategory
-    device  = 'device:' + obj['device']
-    # osはcategory
-    os      = 'os:' + obj['os']
-    # channelはcategory
-    channel = 'channel:' + obj['channel']
-   
-    xs = [feat_index[feat] for feat in [ip_cat, app, device, os, channel]]
-    xs.insert(0, ip_freq_lin) 
-    Xs.append( xs )
+      ip_cat  = ip_freq[ obj['ip'] ] 
+      ip_cat  = 'ip_cat:{}'.format( len(str(ip_cat)) )
+      ip_freq_lin = math.log( ip_freq[ obj['ip'] ] )
+      # appはcategory
+      app     = 'app:' + obj['app']
+      # deviceはcategory
+      device  = 'device:' + obj['device']
+      # osはcategory
+      os      = 'os:' + obj['os']
+      # channelはcategory
+      channel = 'channel:' + obj['channel']
+     
+      xs = [feat_index[feat] for feat in [ip_cat, app, device, os, channel]]
+      xs.insert(0, ip_freq_lin) 
+      Xs.append( xs )
 
-  data = gzip.compress( pickle.dumps( (Xs, ys) ) )
-  open('files/test.pkl.gz', 'wb').write( data )
+    data = gzip.compress( pickle.dumps( (Xs, ys) ) )
+    open(f'files/test_{key:09d}.pkl.gz', 'wb').write( data )
+  args = [(index,path) for index, path in enumerate(Path('./files/data/').glob('test_*'))]
+  with concurrent.futures.ProcessPoolExecutor(max_workers=16) as exe:
+    exe.map( _map_test, args )
 
   # train data
-  fp = open('../../.kaggle/competitions/talkingdata-adtracking-fraud-detection/mnt/ssd/kaggle-talkingdata2/competition_files/train.csv')
-  head = next(fp).strip().split(',')
-  Xs, ys = [], []
-  for index, line in enumerate(fp):
-    if index%100000 == 0:
-      print(f'now train_valid iter {index}')
-    val = line.strip().split(',')
-    obj = dict( zip(head, val) )
-    is_attributed = obj['is_attributed']
+  def _map_train(arg):
+    key, path = arg
+    Xs, ys = [], []
+    for index, line in enumerate(path.open()):
+      if index%100000 == 0:
+        print(f'now train_valid iter {index}@{key}')
+      obj = json.loads( line.strip() )
 
-    y = 1.0 if is_attributed == '1' else 0.0
+      is_attributed = obj['is_attributed']
 
-    del obj['is_attributed'] 
+      y = 1.0 if is_attributed == '1' else 0.0
 
-    ip_cat  = ip_freq[ obj['ip'] ] 
-    ip_cat  = 'ip_cat:{}'.format( len(str(ip_cat)) )
-    ip_freq_lin = math.log( ip_freq[ obj['ip'] ] )
-    # appはcategory
-    app     = 'app:' + obj['app']
-    # deviceはcategory
-    device  = 'device:' + obj['device']
-    # osはcategory
-    os      = 'os:' + obj['os']
-    # channelはcategory
-    channel = 'channel:' + obj['channel']
-   
-    xs = [feat_index[feat] for feat in [ip_cat, app, device, os, channel]]
-    xs.insert(0, ip_freq_lin) 
-    Xs.append( xs ); ys.append( y )
+      del obj['is_attributed'] 
 
-  data = gzip.compress( pickle.dumps( (Xs, ys) ) )
-  open('files/train_valid.pkl.gz', 'wb').write( data )
+      ip_cat  = ip_freq[ obj['ip'] ] 
+      ip_cat  = 'ip_cat:{}'.format( len(str(ip_cat)) )
+      ip_freq_lin = math.log( ip_freq[ obj['ip'] ] )
+      # appはcategory
+      app     = 'app:' + obj['app']
+      # deviceはcategory
+      device  = 'device:' + obj['device']
+      # osはcategory
+      os      = 'os:' + obj['os']
+      # channelはcategory
+      channel = 'channel:' + obj['channel']
+     
+      xs = [feat_index[feat] for feat in [ip_cat, app, device, os, channel]]
+      xs.insert(0, ip_freq_lin) 
+      Xs.append( xs ); ys.append( y )
+
+    data = gzip.compress( pickle.dumps( (Xs, ys) ) )
+    open(f'files/train_valid_{key:09d}.pkl.gz', 'wb').write( data )
+  
+
+  args = [(index,path) for index, path in enumerate(Path('./files/data/').glob('train_*'))]
+  #_map_train(args[0]) 
+  with concurrent.futures.ProcessPoolExecutor(max_workers=16) as exe:
+    exe.map( _map_train, args )
   
