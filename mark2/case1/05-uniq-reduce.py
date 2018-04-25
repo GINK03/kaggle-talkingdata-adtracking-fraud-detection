cf15e6e469dbea9c5e346eb73e6058149eeb62ff
nardtree <gim.kobayashi@gmail.com>
2018-04-25 11:08:37 +0900

    update

---
+++ mark2/case1/05-uniq-reduce.py
@@ -0,0 +1,34 @@
+
+from pathlib import Path
+
+import gzip, pickle
+
+import os
+keys = []
+for name in Path('./var/03').glob('*'):
+  key = str(name).split('/').pop()
+  if os.path.exists(f'var/{key}_all.pkl.gz'):
+    continue
+  keys.append( key )
+
+import concurrent.futures
+
+def pmap(key):
+  print(key)
+  p_freq = {}
+  for name in sorted(Path(f'var/03/{key}').glob('*')):
+    obj = pickle.loads(gzip.decompress(name.open('rb').read()))
+    #kprint(key, obj)
+    for p, space in obj.items():
+      if p_freq.get(p) is None:
+        p_freq[p] = set()
+      p_freq[p].union(space)
+
+  for p in p_freq.keys():
+    p_freq[p] = len(p_freq)
+  print( key, len(p_freq) )
+  d = gzip.compress(pickle.dumps(p_freq))
+  open(f'var/{key}_all.pkl.gz', 'wb').write( d )
+
+with concurrent.futures.ProcessPoolExecutor(max_workers=16) as exe:
+  exe.map(pmap, keys)
