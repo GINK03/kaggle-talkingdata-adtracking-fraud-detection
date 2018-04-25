714cf494677454746b69883412756d617f56c999
nardtree <gim.kobayashi@gmail.com>
2018-04-25 14:43:21 +0900

    update

--- mark2/case1/01-chunk.py
+++ mark2/case1/01-chunk.py
@@ -2,7 +2,6 @@ import datetime
 import itertools
 import sys
 fp = open('../../input/train.csv')
-fp2 = open('../../input/test.csv')
 
 
 key_fp = {}
@@ -13,7 +12,7 @@ control = 0
 for index, line in enumerate(fp):
   if index%10000 == 0:
     print(f'now index {index}')
-  key = index//500000
+  key = control//500000
   line = line.strip()
   raw = line.split(',')[5]
   dt = datetime.datetime.strptime(raw, '%Y-%m-%d %H:%M:%S')
@@ -24,12 +23,13 @@ for index, line in enumerate(fp):
   key_fp[key].write( text + '\n')
   control += 1
 
+fp2 = open('../../input/test.csv')
 head = next(fp2).strip()
 open('var/head_test', 'w').write( 'control_index,'+ head +',wday,day,hour,minute')
 for index, line in enumerate(fp2):
   if index%10000 == 0:
     print(f'now test index {index}')
-  key = index//500000
+  key  = f'test_{control//500000}'
   line = line.strip()
   raw = line.split(',')[6]
   dt = datetime.datetime.strptime(raw, '%Y-%m-%d %H:%M:%S')
