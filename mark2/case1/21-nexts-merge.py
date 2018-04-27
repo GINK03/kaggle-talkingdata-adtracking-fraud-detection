2dc358731999ccae222e9d8378903fd9d1f6b656
nardtree <gim.kobayashi@gmail.com>
2018-04-27 07:28:43 +0000

    update

--- mark2/case1/21-nexts-merge.py
+++ mark2/case1/21-nexts-merge.py
@@ -1,18 +1,22 @@
 
 import os
 import glob
-
+import sys
 # 間違ってたら手動で削除して
-for name in glob.glob('var/*_nextclick.csv'):
+files = sum([sorted(glob.glob('var/*_nextclick.csv')), sorted(glob.glob('var/*_nextnextclick.csv')), sorted(glob.glob('var/*_prevclick.csv'))], [])
+counts = set()
+for name in files:
   count = os.popen(f'cat {name} | wc -l ').read().strip()
   print(name, count)
+  counts.add(count)
 
-fps = [open(name) for name in sum([sorted(glob.glob('var/*_nextclick.csv')), sorted(glob.glob('var/*_nextnextclick.csv')), sorted(glob.glob('var/*_prev.csv'))], [])]
+fps = [open(name) for name in files]
 
 control = 0
-head = 'control_index,' + ','.join( [name.split('/').pop().replace('.csv', '') for name in \
-  sum([sorted(glob.glob('var/*_nextclick.csv')), sorted(glob.glob('var/*_nextnextclick.csv')), sorted(glob.glob('var/*_prev.csv'))], [])] )
-
+head = 'control_index,' + ','.join( [name.split('/').pop().replace('.csv', '') for name in files] )
+if len(counts) != 1:
+  print('何か間違っています')
+  sys.exit()
 open('var/head_nextclicks', 'w').write( head )
 ft = open(f'var/all_nextclick.csv', 'w')
 ft.write( head + '\n' )
